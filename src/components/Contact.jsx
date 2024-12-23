6559870ab3fd79a91e726d3b90e5ee2195455fa6
AmeySwapra <ameyswapratechnologies@gmail.com>
2024-12-23 14:42:17 +0530

    responsive made

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -23,15 +23,26 @@ import {
 
 const Contact = () => {
   return (
-    <Flex direction={{ base: 'column', md: 'row' }}  px={{ base: 4, md: 8, lg: 24 }} py={{ base: 2, md: 3 }} gap={8}>
-     
-      <Box flex="1"  p={6} borderRadius="md" boxShadow="md">
-        <Heading as="h2" size="lg" mb={6}  textAlign="start" color="yellow.500">
+    <Flex
+      direction={{ base: 'column', md: 'row' }}
+      px={{ base: 4, md: 8, lg: 24 }}
+      py={{ base: 6, md: 8 }}
+      gap={8}
+    >
+      {/* Contact Info Box */}
+      <Box
+        flex="1"
+        p={{ base: 4, md: 6 }}
+        borderRadius="md"
+        boxShadow="md"
+        mb={{ base: 8, md: 0 }}
+      >
+        <Heading as="h2" size="lg" mb={6} textAlign="start" color="yellow.500">
           Contact Info
         </Heading>
-        <VStack align="start" spacing={8} mt={'50px'}> 
-       
-          <HStack spacing={12} align="start"> 
+        <VStack align="start" mt={'50px'} spacing={6}>
+          {/* Phone and Email */}
+          <HStack spacing={12} align="start" wrap="wrap">
             <HStack>
               <Icon as={FaPhone} boxSize={6} color="yellow.500" />
               <Box>
@@ -52,8 +63,8 @@ const Contact = () => {
             </HStack>
           </HStack>
 
-         
-          <HStack align="start"  mt={'50px'}>
+          {/* Address */}
+          <HStack spacing={4} mt={'50px'} align="start">
             <Icon as={FaMapMarkerAlt} boxSize={6} color="yellow.500" />
             <Box>
               <Heading as="h3" size="sm" color="gray.700">
@@ -68,29 +79,50 @@ const Contact = () => {
           </HStack>
         </VStack>
 
-       
-        <HStack spacing={2}  mt={'50px'} justify="start"> 
-          <Icon as={FaFacebook} boxSize={'50px'} color="blue.600" /> 
-          <Icon as={FaTwitter} boxSize={'50px'} color="blue.400" /> 
-          <Icon as={FaGooglePlus} boxSize={'50px'} color="yellow.500" />
-          <Icon as={FaPinterest} boxSize={'50px'} color="red.600" />
+        {/* Social Icons */}
+        <HStack mt={'50px'} spacing={4}  justify="start" wrap="wrap">
+          <Icon as={FaFacebook} boxSize="50px" color="blue.600" />
+          <Icon as={FaTwitter} boxSize="50px" color="blue.400" />
+          <Icon as={FaGooglePlus} boxSize="50px" color="yellow.500" />
+          <Icon as={FaPinterest} boxSize="50px" color="red.600" />
         </HStack>
       </Box>
 
-     
-      <Box flex="1"  p={6} borderRadius="md" boxShadow="md">
+      {/* Send A Message Box */}
+      <Box
+        flex="1"
+        p={{ base: 4, md: 6 }}
+        borderRadius="md"
+        boxShadow="md"
+      >
         <Heading as="h2" size="lg" mb={4} textAlign="center" color="red.500">
           Send A Message
         </Heading>
         <VStack spacing={4}>
-          <Input placeholder="Name *" size="lg" focusBorderColor="red.500" />
-          <Input placeholder="Email *" size="lg" focusBorderColor="red.500" />
-          <Input placeholder="Subject" size="lg" focusBorderColor="red.500" />
+          <Input
+            placeholder="Name *"
+            size="lg"
+            focusBorderColor="red.500"
+            mb={2}
+          />
+          <Input
+            placeholder="Email *"
+            size="lg"
+            focusBorderColor="red.500"
+            mb={2}
+          />
+          <Input
+            placeholder="Subject"
+            size="lg"
+            focusBorderColor="red.500"
+            mb={2}
+          />
           <Textarea
             placeholder="Message"
             size="lg"
             focusBorderColor="red.500"
             rows={5}
+            mb={4}
           />
           <Button colorScheme="red" size="lg" w="full">
             Send Message
